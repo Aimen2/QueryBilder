38e298be77baefe1e16f7b208e8814d5ffa2c453
Aimen2 <113955014+Aimen2@users.noreply.github.com>
2023-09-23 17:48:52 -0400

    Add files via upload

---
+++ BannedGame.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryBuilder
+{
+    // Models/BannedGame.cs
+
+    public class BannedGame : Models.IClassModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Reason { get; set; }
+        public DateTime DateBanned { get; set; }
+
+        // Add any other properties specific to the BannedGame table if needed
+    }
+
+}
---
+++ Pokemon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryBuilder
+{
+    // Models/Pokemon.cs
+
+    public class Pokemon : Models.IClassModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Level { get; set; }
+
+        // Add any other properties specific to the Pokemon table if needed
+    }
+
+}
